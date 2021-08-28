05ab279b03bfeed66b6c0e12f52a854e088f3914
Semakov Andrey <semakov.andrey.web@yandex.ru>
2021-08-28 16:45:57 +0400

    Remake installation

--- install.js
+++ install.js
@@ -2,15 +2,19 @@
 
 import { basename, resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { copyFileSync, existsSync, mkdirSync } from 'fs';
 import ncp from 'ncp';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const projectFolder = basename(resolve(__dirname, '../')) !== 'node_modules'
-  ? `./tmp/`
-  : './';
+const projectFolder = basename(resolve(__dirname, '../')) !== 'node_modules' ? `./tmp/` : './';
+const packagePath = (folder) => resolve(__dirname, folder);
+const projectPath = (folder) => resolve(projectFolder, folder);
 
-export const packagePath = (folder) => resolve(__dirname, folder);
+const files = packagePath('files');
+const target = projectPath('./');
+const gi = packagePath('files/.gitignore');
+const targetgi = projectPath('.gitignore');
 
-export const projectPath = (folder) => resolve(projectFolder, folder);
-
-ncp(packagePath('files'), projectPath('./'));
+if (!existsSync(target)) mkdirSync(target);
+ncp(files, target);
+copyFileSync(gi, targetgi);
