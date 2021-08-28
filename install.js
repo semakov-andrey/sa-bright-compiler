a129cbda26cc130ed20896b616e3622731227ab9
Semakov Andrey <semakov.andrey.web@yandex.ru>
2021-08-28 18:46:07 +0400

    Work with package.json

--- install.js
+++ install.js
@@ -2,8 +2,9 @@
 
 import { basename, resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { copyFileSync, renameSync } from 'fs';
+import { copyFileSync, renameSync, readFileSync, existsSync, writeFileSync } from 'fs';
 import ncp from 'ncp';
+import deepmerge from 'deepmerge';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const projectFolder = basename(resolve(__dirname, '../')) !== 'node_modules' ? `./tmp/` : './';
@@ -12,7 +13,17 @@ const projectPath = (folder) => resolve(projectFolder, folder);
 
 const files = packagePath('files');
 const target = projectPath('./');
-const gi = projectPath('.ggitignore');
+const gi = projectPath('.gitignore.data');
 const targetgi = projectPath('.gitignore');
+const pj = projectPath('package.data.json');
+const targetpj = projectPath('package.json');
 
-ncp(files, target, () => renameSync(gi, targetgi));
+ncp(files, target, () => {
+  renameSync(gi, targetgi);
+  if (!existsSync(targetpj)) return;
+  const targetjson = deepmerge(
+    JSON.parse(readFileSync(pj)), 
+    JSON.parse(readFileSync(targetpj))
+  );
+  writeFileSync(targetpj, JSON.stringify(targetjson));
+});
