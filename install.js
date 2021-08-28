7475961f0f8f71edaf76bfa2e3384a528d0c321c
Semakov Andrey <semakov.andrey.web@yandex.ru>
2021-08-28 18:56:49 +0400

    Update package.json

--- install.js
+++ install.js
@@ -2,7 +2,7 @@
 
 import { basename, resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { copyFileSync, renameSync, readFileSync, existsSync, writeFileSync } from 'fs';
+import { copyFileSync, renameSync, readFileSync, existsSync, writeFileSync, unlinkSync } from 'fs';
 import ncp from 'ncp';
 import deepmerge from 'deepmerge';
 
@@ -10,6 +10,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const projectFolder = basename(resolve(__dirname, '../')) !== 'node_modules' ? `./tmp/` : './';
 const packagePath = (folder) => resolve(__dirname, folder);
 const projectPath = (folder) => resolve(projectFolder, folder);
+
+const { version } = JSON.parse(readFileSync(packagePath('./package.json')));
 
 const files = packagePath('files');
 const target = projectPath('./');
@@ -21,9 +23,14 @@ const targetpj = projectPath('package.json');
 ncp(files, target, () => {
   renameSync(gi, targetgi);
   if (!existsSync(targetpj)) return;
-  const targetjson = deepmerge(
-    JSON.parse(readFileSync(pj)), 
-    JSON.parse(readFileSync(targetpj))
-  );
+  const targetjson = deepmerge.all([
+    JSON.parse(readFileSync(targetpj)),
+    { scripts: {
+      'install-compiler': `npx sa-bright-compiler@${version}`,
+      'update-compiler': 'npx sa-bright-compiler'
+    } },
+    JSON.parse(readFileSync(pj))
+  ]);
   writeFileSync(targetpj, JSON.stringify(targetjson, null, 2));
+  unlinkSync(pj);
 });
